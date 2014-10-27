4eed9890f33b7fb2f395c7dc4cfc48a2a80e3b29
JamesWongAndroid <james.wong.android@gmail.com>
2014-10-26 22:06:53 -0400

    indexcontroller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,22 +10,82 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $shirtOrderForm = new Application_Model_DbTable_ShirtOrderForm();
+        $this->view->shirtorderform = $shirtOrderForm->fetchAll();
     }
 
     public function addAction()
     {
-        // action body
+        $form = new Application_Form_ShirtOrderForm();
+        $form->submit->setLable('Add');
+        $this->view->form = $form;
+
+        if ($this->getRequest()->$isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if ($form->isValid($formData)) {
+                $firstName = $form->getValue('firstname');
+                $lastName = $form->getValue('lastname');
+                $address = $form->getValue('address');
+                $shirtSize = $form->getValue('shirtsize');
+                $shirtColor = $form->getValue('color');
+                $shirtType = $form->getValue('shirttype');
+                $shirtOrderForm->addOrderForm($firstName, $lastName, $address, $shirtSize, $shirtColor, $shirtType);
+
+                $this->_helper->redirector('index');
+            } else {
+                $form->populate($formData);
+            }
+
+        }
     }
 
     public function editAction()
     {
-        // action body
+        $form = new Application_Form_ShirtOrderForm();
+        $form->submit->setLabel('Save');
+        $this->view->form = $form;
+
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if ($form->isValid($formData)) {
+                $firstName = $form->getValue('firstname');
+                $lastName = $form->getValue('lastname');
+                $address = $form->getValue('address');
+                $shirtSize = $form->getValue('shirtsize');
+                $shirtColor = $form->getValue('color');
+                $shirtType = $form->getValue('shirttype');
+                $orderShirtForm = new Application_Model_DbTable_ShirtOrderForm();
+                $orderShirtForm->updateOrderForm($firstName, $lastName, $address, $shirtSize, $shirtColor, $shirtType);
+
+                $this->_helper->redirector('index');
+            } else {
+                $form->populate($formData);
+            }
+        } else {
+            $id = $this->_getParam('id', 0);
+            if ($id > 0) {
+                $orderShirtForm = new Application_Model_DbTable_ShirtOrderForm();
+                $form->populate($orderShirtForm->getOrderForm($id));
+
+            }
+        }
     }
 
     public function deleteAction()
     {
-        // action body
+        if ($this->getRequest()->isPost()) {
+            $del = $this->getRequest()->getPost('del');
+            if ($del == 'Yes') {
+                $id = $this->getRequest()->getPost('id');
+                $orderShirtForm = new Application_Model_DbTable_ShirtOrderForm();
+                $orderShirtForm->deleteOrderForm($id);
+            }
+            $this->_helper->redirector('index');
+        } else {
+            $id = $this->_getParam('id', 0);
+            $orderShirtForm = new Application_Model_DbTable_ShirtOrderForm();
+            $this->view->shirtorderform = $shirtorderform->getOrderForm($id);
+        }
     }
 
 

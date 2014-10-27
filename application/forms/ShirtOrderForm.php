18ae2349ae49e883544ebf14739097e650b55a39
JamesWongAndroid <james.wong.android@gmail.com>
2014-10-26 21:41:53 -0400

    zf create form

---
+++ application/forms/ShirtOrderForm.php
@@ -0,0 +1,58 @@
+<?php
+
+class Application_Form_ShirtOrderForm extends Zend_Form
+{
+
+    public function init()
+    {
+        $this->setName('shirtorderform');
+
+        $id = new Zend_Form_Element_Hidden('id');
+        $id->addFilter('Int');
+
+        $firstName = new Zend_Form_Element_Text('firstname');
+        $firstName->setLabel('First Name')
+        		  ->setRequired(true)
+        		  ->addFilter('StripTags')
+        		  ->addFilter('StringTrim')
+        		  ->addValidator('NotEmpty'); 
+
+        $lastName = new Zend_Form_Element_Text('lastname');
+        $lastName->setLabel('Last Name')
+        		  ->setRequired(true)
+        		  ->addFilter('StripTags')
+        		  ->addFilter('StringTrim')
+        		  ->addValidator('NotEmpty');
+
+        $address = new Zend_Form_Element_Text('address');
+        $address->setLabel('address')
+        		  ->setRequired(true)
+        		  ->addFilter('StripTags')
+        		  ->addFilter('StringTrim')
+        		  ->addValidator('NotEmpty');
+
+        $shirtSize = new Zend_Form_Element_Text('shirtsize');
+        $shirtSize->setLabel('Shirt Size')
+        		  ->setRequired(true)
+        		  ->addFilter('StripTags')
+        		  ->addFilter('StringTrim')
+        		  ->addValidator('NotEmpty'); 
+
+        $shirtColor = new Zend_Form_Element_Text('color');
+        $shirtColor->setLabel('Shirt Color')
+        		  ->setRequired(true)
+        		  ->addFilter('StripTags')
+        		  ->addFilter('StringTrim')
+        		  ->addValidator('NotEmpty');  
+
+        $shirtType = new Zend_Form_Element_Text('shirttype');
+        $shirtType->setLabel('Shirt Type')
+        		  ->setRequired(true)
+        		  ->addFilter('StripTags')
+        		  ->addFilter('StringTrim')
+        		  ->addValidator('NotEmpty');  
+    }
+
+
+}
+
